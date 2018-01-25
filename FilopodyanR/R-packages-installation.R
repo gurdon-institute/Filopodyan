29e87abdc0c15a2bc3a151c5124701b5d55b9b62
vu203 <19283990+vu203@users.noreply.github.com>
2018-01-25 12:49:14 +0100

    Upload code for R package installation

---
+++ FilopodyanR/R-packages-installation.R
@@ -0,0 +1,15 @@
+# In order to download required packages, download this script, open it in R, and
+# run (execute) the script. You can monitor the progress of installation in the 
+# main R console window.
+
+install.packages('corrplot', dependencies= TRUE, repos='http://cran.rstudio.com/') 
+install.packages('psych', dependencies= TRUE, repos='http://cran.rstudio.com/') 
+install.packages("wavethresh", dependencies=TRUE, repos="http://cran.rstudio.com/")
+install.packages("Hmisc", dependencies=TRUE, repos="http://cran.rstudio.com/")
+install.packages('RColorBrewer', dependencies= TRUE, repos='http://cran.rstudio.com/') 
+install.packages('purrr', dependencies= TRUE, repos='http://cran.rstudio.com/') 
+install.packages('yarrr', dependencies= TRUE, repos='http://cran.rstudio.com/') 
+install.packages('tidyr', dependencies= TRUE, repos='http://cran.rstudio.com/') 
+install.packages('gplots', dependencies= TRUE, repos='http://cran.rstudio.com/') 
+install.packages('effsize', dependencies= TRUE, repos='http://cran.rstudio.com/') 
+
